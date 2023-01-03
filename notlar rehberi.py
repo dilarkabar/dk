e9d5f6cad2edd9bbe64bd18ac704082a672146a8
Dilara <110340739+dilarkabar@users.noreply.github.com>
2023-01-03 22:13:23 +0300

    Add files via upload
    öğrencilere yardımcı :D

---
+++ notlar rehberi.py
@@ -0,0 +1,30 @@
+import os
+
+
+print("*****hello dear friend, I want to help your poınts.*****")
+
+poınt=int(input("point: "))
+
+def examsrate():
+    
+
+    if poınt<=100 and poınt>=88:
+        print("your exam poınt is so good.you clever panda./AA ")
+    if poınt<=87 and poınt>=81:
+        print(" you good woowo!!!!./BA")
+    if poınt<=74 and poınt>=80:
+        print("you good")
+    if poınt<=73 and poınt>=67:
+        print(":/ :/ you will be good./BB")
+    if poınt<=66 and poınt>=60:
+        print("can be better./CB")
+    if poınt<=59 and poınt>=55:
+        print("not bad./CC")
+    if poınt<=52 and poınt>=46:
+        print("do you believe yourself???*?*?./DC")
+    if poınt<=45 and poınt>=0:
+        print("actually .you are not good right .you need cafe and music maybe ,you want to cry..../FD-DD OR FF")
+examsrate()
+
+ 
+
